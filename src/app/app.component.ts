d513b17c3b042e84d01fd5559e770ac5bc454f79
landesprit <julien.bataille40@gmail.com>
2024-09-25 16:41:56 +0200

    MAJ route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,18 @@
-import { MonsterService } from './services/monster/monster.service';
 import { Component, computed, effect, inject, model, signal } from '@angular/core';
-import { PlayingCardComponent } from './components/playing-card/playing-card.component';
-import { Monster } from './models/monster.model';
-import { SearchBarComponent } from "./components/search-bar/search-bar.component";
-import { MonsterType } from './utils/monster.utils';
-import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,PlayingCardComponent, SearchBarComponent],
+  imports: [RouterOutlet],
   templateUrl: "./app.component.html",
   styleUrl :"./app.component.css"
 
 })
 export class AppComponent {
 
-  monsterService =inject(MonsterService)
-  monsters = signal<Monster[]>([])
-  search = model('');
-
-  filteredMonsters = computed(()=>{
-    return this.monsters().filter(monster => monster.name.includes(this.search()))
-  })
-
-
-  constructor(){
-    this.monsters.set(this.monsterService.getAll())
-  }
-
-  addMonster(){
-    const genericMonster = new Monster();
-    this.monsterService.add(genericMonster);
-    this.monsters.set(this.monsterService.getAll())
-  }
+  
 
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { MonsterListComponent } from './pages/monster-list/monster-list.component';
+import { MonsterComponent } from './pages/monster/monster.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path:'',
+    redirectTo:'home',
+    pathMatch: 'full'    
+},{
+    path: 'monster',
+    component: MonsterComponent
+},{
+    path: 'home',
+    component: MonsterListComponent
+},{
+    path : '**',
+    component: NotFoundComponent
+}];
