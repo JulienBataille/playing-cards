8003e52f27745b40a322f3fe048fb11bf264ddc1
landesprit <julien.bataille40@gmail.com>
2024-09-25 11:04:06 +0200

    MAJ 25/09

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { PlayingCardComponent } from './components/playing-card/playing-card.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [PlayingCardComponent],
+  templateUrl: "./app.component.html",
+  styleUrl :"./app.component.css"
+
 })
 export class AppComponent {
-  title = 'playing-cards';
+
 }
